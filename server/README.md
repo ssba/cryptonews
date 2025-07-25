02906461d7409cd2999f95210bc4be5ffd7749a0
Samvel <s.budumyan@gmail.com>
2025-07-26 01:51:36 +0400

    Server side + Dockerized ones

---
+++ server/README.md
@@ -0,0 +1,4 @@
+# crypto.news test task
+
+Docker command: API_KEY=key APP_DEBUG=true docker compose up --build
+API_KEY is a coingecko api key
---
+++ server/src/index.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const axios = require('axios')
+const cors = require('cors')
+const app = express()
+const port = 3000
+const COINGECKO_API_URL = process.env.COINGECKO_API_URL || 'https://api.coingecko.com/api/v3';
+const DEBUG = process.env.APP_DEBUG === 'true' || true;
+const { createClient } = require('redis');
+
+const redis = createClient({
+  socket: {
+    host: process.env.REDIS_HOST || 'redis',
+    port: process.env.REDIS_PORT || 6379,
+  }
+});
+
+redis.on('error', err => console.log('Redis Client Error', err));
+
+(async () => {
+  await redis.connect().then(() => {
+    if(DEBUG){
+      console.log('Connected to Redis');
+    }
+  }).catch(err => {
+    if(DEBUG){
+      console.error('Failed to connect to Redis:', err);
+    }
+  })
+
+  app.listen(port, () => {
+      console.log(`App running on port ${port}`)
+  })
+})();
+
+app.use(cors())
+
+function debugMessage(message) {
+  if (DEBUG) {
+    console.log(message);
+  }
+}
+
+async function fetchCoin(id) {
+
+  debugMessage(`Fetching data from coingecko : ${id}`);
+
+  try {
+    const response = await axios.get(`${COINGECKO_API_URL}/coins/${id}`, {
+        params: {
+            x_cg_demo_api_key: process.env.API_KEY
+        }
+    });
+
+    return response.data;
+  } catch (error) {
+    throw new Error(`Failed to fetch data for ID ${id}: ${error.message}`);
+  }
+}
+
+app.get('/price/:id', async (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).json({ error: 'ID parameter is required' });
+  }
+
+  debugMessage(`Fetching data for : ${id}`);
+  
+  try {
+
+    debugMessage(`Attempting to fetch data for ${id} from Redis cache`);
+    
+    const cached = await redis.get(id);
+    if (cached) {
+      debugMessage(`Cache hit`);
+      return res.json(JSON.parse(cached));
+    }else {
+      debugMessage(`No cache`);
+    }
+
+    const data = await fetchCoin(id);
+
+    let response = {
+      id: data.id,
+      symbol: data.symbol,
+      name: data.name,
+      current_price: data.market_data.current_price.usd,
+    };
+
+    debugMessage(`Setting cache for ${id} in Redis`);
+    
+    await redis.setEx(id, 60, JSON.stringify(response));
+    res.json(response);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
